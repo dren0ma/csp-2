3960310ad9c5ff225b1bd0eb9f7becfc13a6a1d2
Patrick Legaspi <patrickglegaspi@gmail.com>
2018-02-02 16:00:53 +0800

    modified csp2

--- updateprofile.php
+++ updateprofile.php
@@ -10,22 +10,21 @@ $address = $_POST['address'];
 $email = $_POST['email'];
 $password = sha1($_POST['password']);
 
-$sql = "INSERT INTO users (firstName, lastName, address, email) VALUES ('$first', '$last', '$address', '$email')";
+$sql = "UPDATE users SET 
+	firstName = '$first',
+	lastName = '$last',
+	address = '$address',
+	email = '$email'
+	WHERE userId = '$id'";
+
 mysqli_query($dbcon, $sql) or die(mysqli_error($dbcon));
 
-$id = mysqli_insert_id($dbcon);	/* get userId*/
 
-$sql2 = "INSERT INTO accounts (userName, password, acctType, userId) VALUES ('$userName', '$password', '2', '$id')";
-mysqli_query($dbcon, $sql2) or die(mysqli_error($dbcon));
+$sql = "UPDATE accounts SET 
+	password = '$password'
+	WHERE userId = '$id'";
 
-$id2 = mysqli_insert_id($dbcon);	/* get acctId*/
-
-$sql3 = "INSERT INTO cart (acctId) VALUES ('$id2')";
-mysqli_query($dbcon, $sql3) or die(mysqli_error($dbcon));
-
-$_SESSION['username'] = $userName;
-$_SESSION['acctType'] = '2';
-$_SESSION['acctId'] = $id2;
+mysqli_query($dbcon, $sql) or die(mysqli_error($dbcon));
 
 header('location: index.php');
 
